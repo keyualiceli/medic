a7f87b2ecf309abf942539b220205f3d2b61d8f3
Alice Li <cocolikeyu@gmail.com>
2020-08-20 00:23:36 -0700

    site set-up

--- src/App.js
+++ src/App.js
@@ -5,20 +5,274 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div className="container">
+                <a className="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/navbar-logo.svg" alt="" /></a>
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i className="fa fa-bars ml-1"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav text-uppercase ml-auto">
+                      <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#">Home</a></li>
+                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
+                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#portfolio">Projects</a></li>
+                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#team">Team</a></li>
+                        <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header className="masthead">
+          <div className="container">
+            <div className="intro-text">
+            <div className="intro-heading text-uppercase">M.E.D.I.C.</div>
+              <div className="intro-lead-in">Medical Engineers Designing Innovations for a Cause</div>
+              <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#about">What we do</a>
+            </div>
+          </div>
+        </header>
+        <section className="page-section" id="about">
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">About Us</h2>
+                    <h3 className="section-subheading text-muted">Medic is a ...</h3>
+                </div>
+                <div className="row text-center">
+                    <div className="col-md-4">
+                        <span className="fa-stack fa-4x">
+                            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i className="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 className="my-3">Awareness</h4>
+                        <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+                    <div className="col-md-4">
+                        <span className="fa-stack fa-4x">
+                            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i className="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 className="my-3">Education</h4>
+                        <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+                    <div className="col-md-4">
+                        <span className="fa-stack fa-4x">
+                            <i className="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i className="fa fa-lock fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 className="my-3">Connection</h4>
+                        <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="page-section bg-light" id="portfolio">
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">Projects</h2>
+                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4 col-sm-6 mb-4">
+                        <div className="portfolio-item">
+                            <a className="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                                <div className="portfolio-hover">
+                                    <div className="portfolio-hover-content"><i className="fa fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="assets/img/portfolio/01-thumbnail.jpg" alt="" />
+                            </a>
+                            <div className="portfolio-caption">
+                                <div className="portfolio-caption-heading">Threads</div>
+                                <div className="portfolio-caption-subheading text-muted">Illustration</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-6 mb-4">
+                        <div className="portfolio-item">
+                            <a className="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+                                <div className="portfolio-hover">
+                                    <div className="portfolio-hover-content"><i className="fa fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="assets/img/portfolio/02-thumbnail.jpg" alt="" />
+                            </a>
+                            <div className="portfolio-caption">
+                                <div className="portfolio-caption-heading">Explore</div>
+                                <div className="portfolio-caption-subheading text-muted">Graphic Design</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-6 mb-4">
+                        <div className="portfolio-item">
+                            <a className="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+                                <div className="portfolio-hover">
+                                    <div className="portfolio-hover-content"><i className="fa fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="assets/img/portfolio/03-thumbnail.jpg" alt="" />
+                            </a>
+                            <div className="portfolio-caption">
+                                <div className="portfolio-caption-heading">Finish</div>
+                                <div className="portfolio-caption-subheading text-muted">Identity</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-6 mb-4 mb-lg-0">
+                        <div className="portfolio-item">
+                            <a className="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+                                <div className="portfolio-hover">
+                                    <div className="portfolio-hover-content"><i className="fa fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="assets/img/portfolio/04-thumbnail.jpg" alt="" />
+                            </a>
+                            <div className="portfolio-caption">
+                                <div className="portfolio-caption-heading">Lines</div>
+                                <div className="portfolio-caption-subheading text-muted">Branding</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-6 mb-4 mb-sm-0">
+                        <div className="portfolio-item">
+                            <a className="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+                                <div className="portfolio-hover">
+                                    <div className="portfolio-hover-content"><i className="fa fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="assets/img/portfolio/05-thumbnail.jpg" alt="" />
+                            </a>
+                            <div className="portfolio-caption">
+                                <div className="portfolio-caption-heading">Southwest</div>
+                                <div className="portfolio-caption-subheading text-muted">Website Design</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 col-sm-6">
+                        <div className="portfolio-item">
+                            <a className="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+                                <div className="portfolio-hover">
+                                    <div className="portfolio-hover-content"><i className="fa fa-plus fa-3x"></i></div>
+                                </div>
+                                <img className="img-fluid" src="assets/img/portfolio/06-thumbnail.jpg" alt="" />
+                            </a>
+                            <div className="portfolio-caption">
+                                <div className="portfolio-caption-heading">Window</div>
+                                <div className="portfolio-caption-subheading text-muted">Photography</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className="page-section" id="team">
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
+                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <div className="team-member">
+                            <img className="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="" />
+                            <h4>Kay Garland</h4>
+                            <p className="text-muted">Lead Designer</p>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-envelope"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-facebook-f"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="team-member">
+                            <img className="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="" />
+                            <h4>Larry Parker</h4>
+                            <p className="text-muted">Lead Marketer</p>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-envelope"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-facebook-f"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="team-member">
+                            <img className="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="" />
+                            <h4>Diana Petersen</h4>
+                            <p className="text-muted">Lead Developer</p>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-envelope"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-facebook-f"></i></a>
+                            <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-8 mx-auto text-center"><p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
+                </div>
+            </div>
+        </section>
+        <div className="py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img className="img-fluid d-block mx-auto" src="assets/img/logos/envato.jpg" alt="" /></a>
+                    </div>
+                    <div className="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img className="img-fluid d-block mx-auto" src="assets/img/logos/designmodo.jpg" alt="" /></a>
+                    </div>
+                    <div className="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img className="img-fluid d-block mx-auto" src="assets/img/logos/themeforest.jpg" alt="" /></a>
+                    </div>
+                    <div className="col-md-3 col-sm-6 my-3">
+                        <a href="#!"><img className="img-fluid d-block mx-auto" src="assets/img/logos/creative-market.jpg" alt="" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section className="page-section" id="contact">
+            <div className="container">
+                <div className="text-center">
+                    <h2 className="section-heading text-uppercase">Contact Us</h2>
+                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+                <form id="contactForm" name="sentMessage" noValidate="novalidate">
+                    <div className="row align-items-stretch mb-5">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div className="form-group">
+                                <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div className="form-group mb-md-0">
+                                <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group form-group-textarea mb-md-0">
+                                <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
+                                <p className="help-block text-danger"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <div id="success"></div>
+                        <button className="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <footer className="footer py-4">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-4 text-lg-left">Copyright © Your Website 2020</div>
+                    <div className="col-lg-4 my-3 my-lg-0">
+                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-envelope"></i></a>
+                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-facebook-f"></i></a>
+                        <a className="btn btn-dark btn-social mx-2" href="#!"><i className="fa fa-linkedin"></i></a>
+                    </div>
+                    <div className="col-lg-4 text-lg-right">
+                        <a className="mr-3" href="#!">Privacy Policy</a>
+                        <a href="#!">Terms of Use</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
   );
 }
