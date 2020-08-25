fdcad4365a7b1f9936e355c815c86ea37940ee18
Alice Li <cocolikeyu@gmail.com>
2020-08-25 16:02:45 -0700

    Updates

---
+++ precache-manifest.7234875e897d3cb47838d11c96f8e003.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5259472a5d9acef1ca2d475facef8c2b",
+    "url": "/medic/index.html"
+  },
+  {
+    "revision": "2b64f5d73b8d9082c95b",
+    "url": "/medic/static/css/main.defe30ab.chunk.css"
+  },
+  {
+    "revision": "5cbe5d52d34f4b6d8c90",
+    "url": "/medic/static/js/2.64942596.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/medic/static/js/2.64942596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b64f5d73b8d9082c95b",
+    "url": "/medic/static/js/main.7cf88a10.chunk.js"
+  },
+  {
+    "revision": "6f7f888cfefd606b5c09",
+    "url": "/medic/static/js/runtime-main.8b2bd827.js"
+  },
+  {
+    "revision": "af796e861a291638900647548b672647",
+    "url": "/medic/static/media/footer-bg.af796e86.jpg"
+  },
+  {
+    "revision": "66bc19e1729a9d06009a28bf2c7a75c4",
+    "url": "/medic/static/media/girl.66bc19e1.jpg"
+  },
+  {
+    "revision": "947ab8a9c1bebfbfa1b123a04a7e2d16",
+    "url": "/medic/static/media/header-bg.947ab8a9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/medic/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8df1faef894c64f84b348e505b8e3a40",
+    "url": "/medic/static/media/navbar-logo.8df1faef.png"
+  },
+  {
+    "revision": "af280118c27944a1037958ff16037bd8",
+    "url": "/medic/static/media/syringe.af280118.jpg"
+  }
+]);
